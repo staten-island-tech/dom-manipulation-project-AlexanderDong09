eff0afdf3fc8bf84784645db79f6e21d1f7afd69
AlexanderDong09 <alexanderdong09@gmail.com>
2024-10-01 12:29:06 -0400

    wat

---
+++ index.js
@@ -0,0 +1,24 @@
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  description: document.querySelector(".card-desc"),
+  items: document.querySelectorAll("li"),
+};
+
+console.log(DOMSelectors.description);
+console.log(DOMSelectors.items);
+
+// define
+function addElement() {
+  // find element with queryselector
+  let form = document.querySelector("form");
+  // listen for click event
+  form.addEventListener("submit", function (event) {
+    // prevent default behavior
+    event.preventDefault();
+    // logging click event
+    console.log(event.target);
+    // get info from the form
+  });
+}
+//call
+addElement();
