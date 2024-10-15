2137c626a0e07bbea5bfecc0fd1979c92533c668
AlexanderDong09 <alexanderdong09@gmail.com>
2024-10-14 20:46:16 -0400

    guh

--- old-index.js
+++ old-index.js
@@ -1,6 +1,7 @@
 const DOMSelectors = {
   cardHeader: document.querySelector(".header"),
   button: document.querySelector(".btn"),
+  delete: document.querySelector(".delete-btn"),
   container: document.querySelector(".container"),
   text: document.getElementById("value"),
   image: document.getElementById("img"),
@@ -9,12 +10,12 @@ const DOMSelectors = {
 function addElement(card, img) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
-    `<div class="card"><h2 class="header">${card}</h2><img src="${img}"></div>`
+    `<div class="card"><h2 class="header">${card}</h2><img src="${img}"><button class="delete-btn" type="button">delete</button></div>`
   );
 }
 
-DOMSelectors.button.addEventListener("click", function (insert) {
-  insert.preventDefault();
+DOMSelectors.button.addEventListener("click", function (event) {
+  event.preventDefault();
 
   const inputValue = DOMSelectors.text.value.trim(); // remove whitespace
   const imageLink = DOMSelectors.image.value;
@@ -27,10 +28,32 @@ DOMSelectors.button.addEventListener("click", function (insert) {
 
   addElement(inputValue, imageLink);
   clearForm();
+
+  removeCard();
 });
 
 function clearForm() {
   document.querySelector("form").reset();
+}
+
+function removeCard() {
+  DOMSelectors.container.addEventListener("click", function (event) {
+    // check if what's being clicked is the delete button
+
+    if (event.target.classList.contains("container")) {
+      DOMSelectors.container.removeChild(event.target);
+    }
+  });
+}
+
+function remove() {
+  DOMSelectors.container.addEventListener("click", function (event) {
+    //if i click something in the container class
+    if (event.target.classList.contains("box")) {
+      //and the div class = box
+      DOMSelectors.container.removeChild(event.target); //then remove that child from the container div
+    }
+  });
 }
 
 // event listener for form
